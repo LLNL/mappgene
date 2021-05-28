005a3b50f3058134227d01944734ef88d76430a5
Joseph Moon <moon15@llnl.gov>
2021-05-28 02:14:54 -0700

    add postprocessing steps

--- mappgene.py
+++ mappgene.py
@@ -101,9 +101,26 @@ if __name__ == '__main__':
         params['sdir'] = work_dir
         params['stdout'] = join(work_dir, 'worker.stdout')
         smart_copy(params['git_dir'], work_dir)
-        for f in glob.glob(join(input_dir, '*.fastq.gz')):
-            smart_copy(f, join(work_dir, 'samples/a/b/raw_data', basename(f)))
-        
+        inputs = glob.glob(join(input_dir, '*.fastq.gz'))
+        work_input_dir = join(work_dir, 'samples/a/b/raw_data')
+
+        # # Deinterleave if only a single FASTQ is found
+        if len(inputs) == 1:
+            f = inputs[0]
+            work_f = join(work_input_dir, basename(f))
+            work_r1 = join(work_input_dir, basename(f).replace('.fastq.gz', '_R1.fastq.gz'))
+            work_r2 = join(work_input_dir, basename(f).replace('.fastq.gz', '_R2.fastq.gz'))
+            work_stat = join(work_input_dir, basename(f).replace('.fastq.gz', '.stat'))
+            fasta = join(work_dir, 'references/PS_1200bp.fasta')
+            smart_copy(f, work_f)
+            run(f"bbduk.sh in={work_f} out1={work_r1} out2={work_r2} ref={fasta} stats={work_stat} " +
+                "k=13 ktrim=l hdist=0 restrictleft=31 statscolumns=5", params)
+            smart_remove(work_f)
+        else:
+            for f in inputs:
+                smart_copy(f, join(work_input_dir, basename(f)))
+        return
+
         # Update sample.tsv with read length
         run(f'sh -c "cd {work_dir} && ./vpipe --dryrun"', params)
         samples_tsv = join(work_dir, 'samples.tsv')
@@ -119,6 +136,21 @@ if __name__ == '__main__':
         ncores = int(math.floor(multiprocessing.cpu_count() / 2))
         run(f'sh -c "cd {work_dir} && ./vpipe --cores {ncores} --use-conda"', params)
         time.sleep(10)
+
+        smart_copy('/usr/WS2/moon15/old/mappgene/output/example/work_dir/samples',
+            '/usr/WS2/moon15/mappgene/output/example/work_dir/samples')
+        vcf_s0 = join(work_dir, 'samples/a/b/variants/SNVs/snvs.vcf')
+        vcf_s1 = join(work_dir, 'samples/a/b/variants/SNVs/snvs_NC_045512.vcf')
+        vcf_s2 = join(work_dir, 'samples/a/b/variants/SNVs/snvs_NC_045512.2.snpEFF.vcf')
+        vcf_s3 = join(work_dir, 'samples/a/b/variants/SNVs/snvs_NC_045512.2.snpSIFT.txt')
+
+        run(f'sh -c "sed "s/MN908947.3/NC_045512.2/g" {vcf_s0} > {vcf_s1}"', params)
+        # run('java -jar /opt/snpEff/snpEff.jar download -v NC_045512.2', params)
+        run(f'sh -c "java -Xmx8g -jar /opt/snpEff/snpEff.jar NC_045512.2 {vcf_s1} > {vcf_s2}"', params)
+        run(f'sh -c "cat {vcf_s2} | /opt/snpEff/scripts/vcfEffOnePerLine.pl | java -jar /opt/snpEff/SnpSift.jar ' + 
+            f' extractFields - CHROM POS REF ALT AF DP "ANN[*].IMPACT" "ANN[*].FEATUREID" "ANN[*].EFFECT" ' + 
+            f' "ANN[*].HGVS_C" "ANN[*].HGVS_P" "ANN[*].CDNA_POS" "ANN[*].AA_POS" "ANN[*].GENE" > {vcf_s3}"', params)
+
         smart_copy(join(work_dir, 'samples/a/b/alignments'), join(output_dir, 'alignments'))
         smart_copy(join(work_dir, 'samples/a/b/variants'), join(output_dir, 'variants'))
         smart_copy(join(work_dir, 'samples/a/b/extracted_data'), join(output_dir, 'extracted_data'), exclude=['*.gz', '*.fasta'])
