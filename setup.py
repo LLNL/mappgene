58a83093871230466dec3e089eb8bf06067d7bc5
Joseph Moon <moon15@llnl.gov>
2021-08-20 01:52:08 -0700

    add fixed versions of deps

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ src_path = join(src_path, 'mappgene')
 setup(
     name='mappgene',
     description='Genomic sequence analysis for HPC',
-    version='0.1.0',
+    version='0.1.2',
     python_requires='>3.7.0',
     author='Joseph Moon',
     author_email='jmoon1506@gmail.com',
@@ -21,7 +21,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     package_data={'mappgene' : ['data/*'] },
-    install_requires=['parsl','pytest'],
+    install_requires=['numpy>=1.21.2', 'parsl>=1.1.0', 'pytest>=6.2.4'],
 )
 
 # ------------------------------------------------------------------------------
