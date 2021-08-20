42f685dbc0fff300571e0d5a7b613067bdfe4e76
Joseph Moon <moon15@llnl.gov>
2021-08-20 02:13:02 -0700

    wip fix dep error

--- setup.py
+++ setup.py
@@ -9,8 +9,8 @@ src_path = join(src_path, 'mappgene')
 setup(
     name='mappgene',
     description='Genomic sequence analysis for HPC',
-    version='0.1.2',
-    python_requires='>3.7.0',
+    version='0.1.5',
+    python_requires='>=3.7',
     author='Joseph Moon',
     author_email='jmoon1506@gmail.com',
     entry_points={
@@ -21,7 +21,17 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     package_data={'mappgene' : ['data/*'] },
-    install_requires=['numpy>=1.21.2', 'parsl>=1.1.0', 'pytest>=6.2.4'],
+    install_requires=['numpy>=1.11.3', 'parsl>=0.9.0', 'pytest>=4.0'],
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
 )
 
 # ------------------------------------------------------------------------------
