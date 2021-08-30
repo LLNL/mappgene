501af0bb614b4fe8898c0bcd3528ed4e18c99ce3
Joseph Moon <moon15@llnl.gov>
2021-08-29 19:35:22 -0700

    increment version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ src_path = join(src_path, 'mappgene')
 setup(
     name='mappgene',
     description='Genomic sequence analysis for HPC',
-    version='0.1.8',
+    version='0.1.9',
     python_requires='>=3.7',
     author='Joseph Moon',
     author_email='moon15@llnl.gov',
